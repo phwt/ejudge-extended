b8d5834b2f27fc20dc4adf9e3c3b8946ee0b1548
phwt.smwt <phwt.smwt@gmail.com>
2019-01-25 23:56:53 +0700

    Base files

---
+++ content.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+    // alert("wow");
+    $(".dl-horizontal").append(`
+        <br><a href="vscode://file/K:\\Files\\GitHub\\CP2018\\lab\\0124\\4276.c">
+            <button class="btn btn-secondary btn-block">View on computer</button>
+        </a>
+    `);
+});
