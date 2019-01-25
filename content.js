d443fd19a31a631db9270938c1495eb5943bf03a
phwt.smwt <phwt.smwt@gmail.com>
2019-01-26 02:25:13 +0700

    Update content.js

--- content.js
+++ content.js
@@ -1,8 +1,25 @@
 $( document ).ready(function() {
-    // alert("wow");
+
     $(".dl-horizontal").append(`
-        <br><a href="vscode://file/K:\\Files\\GitHub\\CP2018\\lab\\0124\\4276.c">
-            <button class="btn btn-secondary btn-block">View on computer</button>
-        </a>
+        <br><button id="ext_open" class="btn btn-secondary btn-block">View on computer</button>
     `);
+
+    $("#ext_open").click(function() {
+        var url = window.location.href;
+        var problem_id = url.substr(url.lastIndexOf('/') + 1);
+        // for(i = 0101; i < 1231+1; i++){
+        //     if(i%100 > 31 || i%100 == 0){
+        //         continue;
+        //     }
+
+        //     var guess = i.toString(10);
+        //     if((i).toString(10).length == 3){
+        //         guess = '0' + guess;
+        //     }
+
+            // console.log("vscode://file/K:\\Files\\GitHub\\CP2018\\lab\\"+guess+"\\"+problem_id+".c");
+        // window.open("vscode://file/K:\\Files\\GitHub\\CP2018\\lab\\"+problem_id+".c","_self");
+        location.href = "vscode://file/K:\\Files\\GitHub\\CP2018\\lab\\"+problem_id+".c";
+        // }
+    });
 });
