81ffc8293826447eff6fafcd3a13e68771d86cb7
phwt.smwt <phwt.smwt@gmail.com>
2019-01-31 19:50:14 +0700

    Add some files
    - Add file contains commandline for executing in terminals.

---
+++ terminals/setup_login.bat
@@ -0,0 +1,18 @@
+@echo off
+curl -s --cookie-jar ./ejudge_cookie https://ejudge.it.kmitl.ac.th/auth/login > temp.txt
+FOR /F "skip=144 delims=" %%A IN (temp.txt) DO (
+    echo %%A > temp.txt
+    goto continue_process
+)
+
+:continue_process
+set /p token=<temp.txt
+set "token=%token:~160,40%"
+echo Token is %token%
+set /p username="Username: "
+powershell -Command $pword = read-host "Password" -AsSecureString ; $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword) ; [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR) > .tmp.txt & set /p password=<.tmp.txt & del .tmp.txt
+curl --cookie ./ejudge_cookie --cookie-jar ./ejudge_cookie -d "username=%username%" -d "password=%password%" -d "_token=%token%" -d "remember=true" https://ejudge.it.kmitl.ac.th/auth/loggedin
+curl --cookie ./ejudge_cookie --cookie-jar ./ejudge_cookie https://ejudge.it.kmitl.ac.th/course/109/enter
+echo %token% > current_token.txt
+del temp.txt
+PAUSE
