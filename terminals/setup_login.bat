c9c86f3bd3f3a3a6088721523427732340d56835
phwt.smwt <phwt.smwt@gmail.com>
2019-02-01 22:09:45 +0700

    Update setup_login.bat
    - Remove token display
    - Save token to current_token file

--- terminals/setup_login.bat
+++ terminals/setup_login.bat
@@ -8,7 +8,7 @@ FOR /F "skip=144 delims=" %%A IN (temp.txt) DO (
 :continue_process
 set /p token=<temp.txt
 set "token=%token:~160,40%"
-echo Token is %token%
+echo %token% > current_token
 set /p username="Username: "
 powershell -Command $pword = read-host "Password" -AsSecureString ; $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword) ; [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR) > .tmp.txt & set /p password=<.tmp.txt & del .tmp.txt
 curl --cookie ./ejudge_cookie --cookie-jar ./ejudge_cookie -d "username=%username%" -d "password=%password%" -d "_token=%token%" -d "remember=true" https://ejudge.it.kmitl.ac.th/auth/loggedin
