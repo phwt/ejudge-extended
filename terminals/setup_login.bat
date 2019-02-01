0ece04a5204850eb8338aa61fe15297287032ec2
phwt.smwt <phwt.smwt@gmail.com>
2019-02-01 20:19:37 +0700

    Add custom course id

--- terminals/setup_login.bat
+++ terminals/setup_login.bat
@@ -12,7 +12,7 @@ echo Token is %token%
 set /p username="Username: "
 powershell -Command $pword = read-host "Password" -AsSecureString ; $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword) ; [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR) > .tmp.txt & set /p password=<.tmp.txt & del .tmp.txt
 curl --cookie ./ejudge_cookie --cookie-jar ./ejudge_cookie -d "username=%username%" -d "password=%password%" -d "_token=%token%" -d "remember=true" https://ejudge.it.kmitl.ac.th/auth/loggedin
-curl --cookie ./ejudge_cookie --cookie-jar ./ejudge_cookie https://ejudge.it.kmitl.ac.th/course/109/enter
-echo %token% > current_token.txt
+set /p cid="Course ID: "
+curl --cookie ./ejudge_cookie --cookie-jar ./ejudge_cookie https://ejudge.it.kmitl.ac.th/course/%cid%/enter
 del temp.txt
 PAUSE
