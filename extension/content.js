611c8a4208a5a8b13a25a6a29c05ac1d5398ad6c
phwt.smwt <phwt.smwt@gmail.com>
2019-02-01 16:23:40 +0700

    Update content.js
    - Remove residue
    - Seperate path to variable

--- extension/content.js
+++ extension/content.js
@@ -5,23 +5,12 @@ $( document ).ready(function() {
             <br><button id="ext_open" class="btn btn-secondary btn-block">View on computer</button>
         `);
 
+        var path = "K:\\Files\\GitHub\\CP2018\\lab\\";
+
         $("#ext_open").click(function() {
             var url = window.location.href;
             var problem_id = url.substr(url.lastIndexOf('/') + 1);
-            // for(i = 0101; i < 1231+1; i++){
-            //     if(i%100 > 31 || i%100 == 0){
-            //         continue;
-            //     }
-
-            //     var guess = i.toString(10);
-            //     if((i).toString(10).length == 3){
-            //         guess = '0' + guess;
-            //     }
-
-                // console.log("vscode://file/K:\\Files\\GitHub\\CP2018\\lab\\"+guess+"\\"+problem_id+".c");
-            // window.open("vscode://file/K:\\Files\\GitHub\\CP2018\\lab\\"+problem_id+".c","_self");
-            location.href = "vscode://file/K:\\Files\\GitHub\\CP2018\\lab\\"+problem_id+".c";
-            // }
+            location.href = "vscode://file/"+ path + problem_id + ".c";
         });
     } else if(window.location.href.includes("quiz")){
         $('[data-toggle="tab"]').each(function(i, obj) {
