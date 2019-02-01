c5191743cc9ab4084d528e48518193910196921e
phwt.smwt <phwt.smwt@gmail.com>
2019-02-01 16:29:54 +0700

    Update content.js
    - Add comments
    - Add Quiz highlighting selected question

--- extension/content.js
+++ extension/content.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
 
+    //Problems page
     if(window.location.href.includes("problem")){
         $(".dl-horizontal").append(`
             <br><button id="ext_open" class="btn btn-secondary btn-block">View on computer</button>
@@ -12,11 +13,20 @@ $( document ).ready(function() {
             var problem_id = url.substr(url.lastIndexOf('/') + 1);
             location.href = "vscode://file/"+ path + problem_id + ".c";
         });
+    
+    //Quizes Page
     } else if(window.location.href.includes("quiz")){
         $('[data-toggle="tab"]').each(function(i, obj) {
             old_text = $(this).html();
             id = $(this).attr("href").slice(-3);
-            $(this).html(old_text + "(" + id + ")");
+            $(this).html(old_text + "- " + id);
+        });
+
+        $('[data-toggle="tab"]').click(function() {
+            $('[data-toggle="tab"]').each(function(i, obj) {
+                $(this).removeClass('btn-primary').addClass('btn-default');
+            });
+            $(this).removeClass('btn-default').addClass('btn-primary');
         });
     }
 
